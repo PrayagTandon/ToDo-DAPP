dd4c12a037107057b6602b764fcdf60be5242a6a
Prayag <tandonprayag99@gmail.com>
2024-11-19 20:48:36 -0500

    Configuting Hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,11 @@
-require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 module.exports = {
-  solidity: "0.8.27",
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: process.env.ALCHEMY_SEPOLIA_URL,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+    }
+  }
 };
