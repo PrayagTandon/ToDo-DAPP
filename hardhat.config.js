ae115ae765e0b4b986962f6a0c682e3d80d3825c
Prayag <tandonprayag99@gmail.com>
2024-11-26 15:45:50 -0500

    Setting up the project

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,12 @@
-require("@nomicfoundation/hardhat-toolbox");
-
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+require("@nomicfoundation/hardhat-ethers");
+require('dotenv').config();
 module.exports = {
-  solidity: "0.8.27",
+  solidity: "0.8.4",
+  networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_SEPOLIA_URL,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY],
+    }
+  }
 };
