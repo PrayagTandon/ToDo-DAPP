c394521629001a8799ffc4cf85d03d23b16b093f
Prayag <tandonprayag99@gmail.com>
2024-11-29 20:49:26 -0500

    ....

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,19 +1,38 @@
-const main = async () => {
-    const contractFactory = await ethers.getContractFactory('TaskContract');
-    const contract = await contractFactory.deploy();
-    await contract.deployed();
+const hre = require("hardhat");
 
-    console.log("Contract deployed to: ", contract.address);
+async function main() {
+    const [deployer] = await hre.ethers.getSigners();
+
+    console.log("Deploying contracts with the account:", deployer.address);
+
+    const TaskContract = await hre.ethers.getContractFactory("TaskContract");
+    const taskContract = await TaskContract.deploy();
+
+    console.log("TaskContract deployed to:", taskContract.target);
 }
 
-const runMain = async () => {
-    try {
-        await main();
-        process.exit(0);
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
-}
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
 
-runMain();
+
+// const main = async () => {
+//     const contractFactory = await ethers.getContractFactory('TaskContract');
+//     const contract = await contractFactory.deploy();
+//     await contract.deployed();
+
+//     console.log("Contract deployed to: ", contract.address);
+// }
+
+// const runMain = async () => {
+//     try {
+//         await main();
+//         process.exit(0);
+//     } catch (error) {
+//         console.log(error);
+//         process.exit(1);
+//     }
+// }
+
+// runMain();
